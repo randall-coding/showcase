99cf7b97a61fdf2243a33d10e6594ee955f12abf
mcgoy <randallcoding@protonmail.com>
2019-11-16 18:32:58 -0700

    patch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  
+
   # config.action_mailer.smtp_settings = {
   #   address: 'smtp.gmail.com',
   #   port: 587,
@@ -52,7 +52,7 @@ Rails.application.configure do
     port:            587,
     authentication: :plain,
     user_name:      'apikey',
-    password:       ENV['SENDGRID_API_KEY']
+    password:       ENV['SENDGRID_KEY']
   }
 
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
     port:            587,
     authentication: :plain,
     user_name:      'apikey',
-    password:       ENV['SENDGRID_API_KEY']
+    password:       ENV['SENDGRID_KEY']
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
