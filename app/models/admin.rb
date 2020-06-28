ec9fff0d49c9b51f8da7ac540e02d8abca4baf9e
mcgoy <randallcoding@protonmail.com>
2020-06-27 21:13:58 -0500

    Destroys old admin model

--- app/models/admin.rb
+++
@@ -1,4 +0,0 @@
-class Admin < ApplicationRecord
-  has_secure_token :token
-  has_secure_password
-end
--- config/routes.rb
+++ config/routes.rb
@@ -21,9 +21,6 @@ Rails.application.routes.draw do
   get 'api/ethereum' => 'transaction#index' , as: :ethereum
   post 'api/ethereum' => 'transaction#index' , as: :ethereum_post
 
-  get '/sign_in' => 'admins#sign_in' , as: :admin_sign_in
-  post '/sign_in' => 'admins#create_session', as: :admin_create_session
-
   get '/portfolio_item/new' => 'portfolio_items#new', as: :portfolio_item
   post '/portfolio_tem/new' => 'portfolio_items#create', as: :portfolio_items
 
