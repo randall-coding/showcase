8156563973da6c02f615779310fd89efba6b9522
mcgoy <randallcoding@protonmail.com>
2020-06-28 17:15:48 -0500

    Code clean up

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -55,105 +55,18 @@ $(document).on("turbolinks:load", function() {
         })
     });
 
-    changeTabColor = function(newTab) {
+    function changeTabColor(newTab) {
       var oldTab;
       oldTab = $('.nav-item-active');
       oldTab.removeClass('nav-item-active');
       return newTab.addClass('nav-item-active');
     };
-    nextSlide = function(imgs) {
-      var $next, $nextnext, speed;
-      $next = 0;
-      $nextnext = 0;
-      speed = 750;
-      if (i < imgs.length - 1) {
-        $next = $(imgs[i + 1]);
-        $nextnext = $(imgs[i + 2]);
-      } else {
-        $next = $(imgs[0]);
-        $nextnext = $(imgs[1]);
-      }
-      if (i + 1 === (imgs.length - 1)) {
-        $nextnext = $(imgs[0]);
-      }
-      $(imgs[i]).animate({
-        left: "-=100%"
-      }, speed, function() {
-        return $(this).removeClass('active').addClass('inactive');
-      });
-      $next.animate({
-        left: "-=100%"
-      }, speed);
-      increment();
-      return $nextnext.addClass('active').removeClass('inactive').css('left', '100%');
-    };
-    nextSlide_old = function(imgs) {
-      if (i < imgs.length - 1) {
-        return $(imgs[i]).animate({
-          left: "-610px"
-        }, function() {
-          $(imgs[i]).css('display', 'none').css('left', '0');
-          $(imgs[i + 1]).css('left', '610px').css('display', 'inline');
-          $(imgs[i + 1]).animate({
-            left: "0px"
-          });
-          return increment();
-        });
-      } else {
-        return $(imgs[i]).animate({
-          left: "-610px"
-        }, function() {
-          $(imgs[i]).css('display', 'none').css('left', '0');
-          $(imgs[0]).css('left', '610px').css('display', 'inline');
-          $(imgs[0]).animate({
-            left: "0px"
-          });
-          return increment();
-        });
-      }
-    };
-    increment = function() {
-      var i;
-      if (i < imgs.length - 1) {
-        return i += 1;
-      } else {
-        return i = 0;
-      }
-    };
-    return nextSlide2 = function(as) {
-      var a, img_current;
-      a = as.find("img.portfolio_active").parent();
-      if (a.next().length !== 0) {
-        img_current = a.find('img');
-        return img_current.animate({
-          left: "-400px"
-        }, function() {
-          img_current.removeClass('portfolio_active').addClass('portfolio');
-          a.next().find("img").css('left', '400px').removeClass('portfolio').addClass('portfolio_active');
-          return a.next().find("img").animate({
-            left: "0px"
-          });
-        });
-      } else {
-        img_current = a.find('img');
-        return img_current.animate({
-          left: "-400px"
-        }, function() {
-          img_current.removeClass('portfolio_active').addClass('portfolio');
-          $(as[0]).find("img").css('left', '400px').removeClass('portfolio').addClass('portfolio_active');
-          return $(as[0]).find("img").animate({
-            left: "0px"
-          });
-        });
-      }
-    };
-  });
 
-  function toggleMoustache(){
-      if ( $('#moustache').css('visibility')=='hidden'){
-          $('#moustache').css('visibility','visible')
-        } else {
-            $('#moustache').css('visibility','hidden')
-          }
-  }
-;
+    function toggleMoustache(){
+        if ( $('#moustache').css('visibility')=='hidden'){
+            $('#moustache').css('visibility','visible')
+          } else {
+              $('#moustache').css('visibility','hidden')
+            }
+    }
+}) //end document.ready()
