48bf31e580084390013ebcbd0afffc91f7d9a27b
mcgoy <randallcoding@protonmail.com>
2020-07-14 22:34:48 -0500

    Changes sign in redirect to admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
 
+  # Active Admin access denied
   def access_denied(exception)
     render file: "#{Rails.root}/public/404.html" , status: 404
+  end
+
+ # Devise sign in redirect  
+  def after_sign_in_path_for(resource)
+    admin_root_path
   end
 end
