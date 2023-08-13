250ae3ebe0c304abd12d8e7a1f382b60bed4892f
Randall Coding <randallcoding@protonmail.com>
2023-08-13 20:16:48 +0700

    Fixes admin panel edit page for image display

--- app/admin/portfolio_items.rb
+++ app/admin/portfolio_items.rb
@@ -33,7 +33,7 @@ ActiveAdmin.register PortfolioItem do
       f.input :position
       if f.object.image?
         # Display the existing image preview
-        f.input :image, as: :file, value: f.object.image.id, hint: image_tag(f.object.image_url, style:"width:200px")
+        f.input :image, as: :file, value: f.object.image, hint: image_tag(f.object.image_url, style:"width:200px")
       else
         f.input :image, as: :file
       end    end
